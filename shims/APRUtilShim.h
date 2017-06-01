bf47f2090204346a6589ffdf0cfb7eba698cb706
Helge Hess <devteam@zeezide.de>
2017-06-01 14:58:58 +0200

    Massage it to compile on Raspberry Pi
    ... a little hack required, needs a proper fix
    eventually.

--- shims/APRUtilShim.h
+++ shims/APRUtilShim.h
@@ -1,6 +1,16 @@
 #ifndef __APRUTIL_SWIFT_SHIM_H__
 #define __APRUTIL_SWIFT_SHIM_H__
 
+#ifdef __linux__
+  /* Hack required on Raspberry Pi, probably due to incorrect -D (which we can't pass to
+   * SPM via pkgconfig).
+   */
+#  ifdef __ARM_EABI__
+     // off64_t is undefined (32bit Xenial on RaspberryPi)
+     typedef signed long long int off64_t;
+#  endif
+#endif
+     
 #include "apu.h"
 #include "apr_buckets.h"
 #include "apr_dbd.h"
